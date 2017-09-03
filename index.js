cdd77a5b2c8d3139c3cfa5f050ae16e77aa66b6a
Yuda Cogati <yudapc@gmail.com>
2017-09-03 14:21:15 +0700

    added function PrintResult

---
+++ index.js
@@ -0,0 +1,7 @@
+exports.PrintResult = function(result, message) {
+  console.log('-------------------------');
+  console.log('Print Result: ');
+  console.log('-------------------------');
+  console.log('Result: ', result);
+  console.log('Message: ', message);
+};
